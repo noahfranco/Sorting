16222a7e695d84cb747403eb859c087461432fdf
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-01-22 10:44:48 -0500

    finished merge and merge_sorting and met MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,7 +5,7 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        for smallest_index in range(cur_index + 1, len(arr)):
+        for smallest_index in range(cur_index + 1, len(arr)): # To target the length and rang of the string
             if arr[smallest_index] < arr[cur_index]:
                 arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
@@ -21,7 +21,7 @@ print(selection_sort([1, 2, 3, 5, 4, 6, 8, 7, 9, 10]))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for i in range(len(arr)-1, 0, -1):
+    for i in range(len(arr)-1, 0, -1): # To -1, 0, -1 is done in a bubble to target the integer on the left and right
         for sorting in range(0, i):
             if arr[sorting] > arr[sorting+1]:
                 arr[sorting], arr[sorting+1] = arr[sorting+1], arr[sorting]
